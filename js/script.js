bbe17a08b707b84ecdb20fbb75cdffd6923d9ea3
satvik204 <78365606+satvik204@users.noreply.github.com>
2024-10-12 19:55:34 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector("#menu-icon");
+let nav = document.querySelector(".navlist");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    nav.classList.remove('active');
+}
+const header = document.querySelector("header")
+window.addEventListener("scroll",function () {
+    header.classList.toggle("sticky",window.scrollY>0);
+})
